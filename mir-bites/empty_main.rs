07b9584145ab7a990cc997f3d4114493016332ca
Patience Shyu <patienceshyu@gmail.com>
2017-03-27 23:03:51 -0700

    empty main

---
+++ mir-bites/empty_main.rs
@@ -0,0 +1,16 @@
+pub fn main() {
+
+}
+
+// MIR
+
+/*
+fn main() -> () {
+    let mut _0: ();                      // return pointer
+
+    bb0: {
+        _0 = ();                         // scope 0 at <anon>:1:15: 3:2
+        return;                          // scope 0 at <anon>:3:2: 3:2
+    }
+}
+*/
