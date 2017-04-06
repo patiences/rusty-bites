a6df43baefac4ce70ce1f383361a8e22a4327282
Patience Shyu <patienceshyu@gmail.com>
2017-04-06 00:10:24 -0700

    [WIP] binary search tree impl

---
+++ bst.rs
@@ -0,0 +1,123 @@
+enum BST {
+    Node(i32, Box<BST>, Box<BST>),
+    Empty
+}
+
+/// Inserts a node with this key into the tree rooted at root. 
+fn insert(root: Box<BST>, key: i32) -> Box<BST> {
+    let copy = *root;
+    return match copy {
+        BST::Empty => Box::new(BST::Node(key, Box::new(BST::Empty),
+Box::new(BST::Empty))),
+        BST::Node(data, left, right) => if key < data {
+            Box::new(BST::Node(data, insert(left, key), right))
+        } else {
+            Box::new(BST::Node(data, left, insert(right, key)))
+        },
+    };
+}
+
+/// Returns the number of nodes in the tree rooted at this node. 
+fn num_nodes(root: &BST) -> i32 {
+    return match *root {
+        BST::Empty => 0,
+        BST::Node(data, ref left, ref right) => 1 + num_nodes(left) +
+num_nodes(right),
+    };
+}
+
+/// Returns the number of leaves in the tree rooted at this node. 
+fn num_leaves(root: &BST) -> i32 {
+    return match *root {
+        BST::Empty => 0, 
+        BST::Node(data, ref left, ref right) => {
+            match **left {
+                BST::Empty => {
+                    match **right {
+                        BST::Empty => return 1, // it's a leaf! 
+                        BST::Node(right_data, ref right_left, ref right_right)
+=> num_leaves(left) + num_leaves(right),
+                    }
+                },
+                BST::Node(left_data, ref left_left, ref left_right) =>
+num_leaves(left) + num_leaves(right),
+            }
+        }
+    };
+}
+
+/// Returns the height of node x. 
+fn height(x: &BST) -> i32 {
+    return match *x {
+        BST::Empty => -1, 
+        BST::Node(data, ref left, ref right) => {
+            let height_left = height(left);
+            let height_right = height(right);
+            if height_left > height_right {
+                return 1 + height_left;
+            }
+            return 1 + height_right;
+        }
+    }
+}
+
+/// Returns the depth of node x in the tree rooted at root. 
+fn depth(root: &BST, x: &BST) -> i32 {
+    return match *x {
+        BST::Empty => -1, 
+        BST::Node(x_data, ref x_left, ref x_right) => {
+            match *root {
+                BST::Empty => -1, 
+                BST::Node(root_data, ref root_left, ref root_right) => {
+                    if x_data == root_data {
+                        return 0;
+                    }
+                    if x_data < root_data {
+                        return 1 + depth(root_left, x);
+                    }
+                    return 1 + depth(root_right, x);
+                }
+            }
+        }
+    }
+}
+
+/// Prints the tree sideways. 
+fn print(root: &BST) -> () {
+    println!("Printing tree: ");
+    print_helper(root, 0)
+}
+
+fn print_helper(root: &BST, depth: i32) -> () {
+    match *root {
+        BST::Empty => (),
+        BST::Node(data, ref left, ref right) => {
+            //println!("{:?}", data);
+            print_helper(right, depth+1);
+            print!("{}", "  ".repeat(depth as usize));
+            println!("{:?}", data);
+            print_helper(left, depth+1);
+    }
+  }
+}
+
+fn main() {
+    let mut tree = Box::new(BST::Empty);
+    tree = insert(tree, 8);
+    tree = insert(tree, 22);
+    tree = insert(tree, 1);
+    print(&tree);
+    println!("Num nodes: {:?}", num_nodes(&tree));
+    println!("Num leaves: {:?}", num_leaves(&tree));
+    println!("Height: {:?}", height(&tree));
+    
+    tree = insert(tree, 17);
+    tree = insert(tree, 20);
+    tree = insert(tree, 2);
+    tree = insert(tree, 9);
+    print(&tree);
+    
+    println!("Num nodes: {:?}", num_nodes(&tree));
+    let x = Box::new(BST::Node(17, Box::new(BST::Empty), Box::new(BST::Empty)));
+    println!("Depth of 17 in tree: {:?}", depth(&tree, &x));
+}
--- mir-bites/vec.rs
+++ mir-bites/vec.rs
@@ -1,49 +1,74 @@
 fn main() {
     let mut vec : Vec<i32> = Vec::new();
     vec.push(1);
+    let first = vec[0];
 }
 
-// MIR
+// MIR 
 
-/*
+/* 
 fn main() -> () {
     let mut _0: ();                      // return pointer
     scope 1 {
         let mut _1: std::vec::Vec<i32>;  // "vec" in scope 1 at <anon>:2:9: 2:16
+        scope 2 {
+            let _5: i32;                 // "first" in scope 2 at <anon>:4:9:
+4:14
+        }
     }
     let mut _2: ();
     let mut _3: ();
     let mut _4: &mut std::vec::Vec<i32>;
+    let mut _6: i32;
+    let mut _7: &i32;
+    let mut _8: &std::vec::Vec<i32>;
 
     bb0: {
         StorageLive(_1);                 // scope 0 at <anon>:2:9: 2:16
-        _1 = <std::vec::Vec<T>><i32>::new() -> bb1; // scope 0 at <anon>:2:30:
+        _1 = const <std::vec::Vec<T>>::new() -> bb1; // scope 0 at <anon>:2:30:
 2:40
     }
 
     bb1: {
         StorageLive(_4);                 // scope 1 at <anon>:3:5: 3:8
         _4 = &mut _1;                    // scope 1 at <anon>:3:5: 3:8
-        _3 = <std::vec::Vec<T>><i32>::push(_4, const 1i32) -> [return: bb4, unwind: bb3]; // scope 1 at <anon>:3:5: 3:16
+        _3 = const <std::vec::Vec<T>>::push(_4, const 1i32) -> [return: bb4,
+unwind: bb3]; // scope 1 at <anon>:3:5: 3:16
     }
 
     bb2: {
-        resume;                          // scope 0 at <anon>:1:1: 4:2
+        resume;                          // scope 0 at <anon>:1:1: 5:2
     }
 
     bb3: {
-        drop(_1) -> bb2;                 // scope 0 at <anon>:4:2: 4:2
+        drop(_1) -> bb2;                 // scope 0 at <anon>:5:2: 5:2
     }
 
     bb4: {
         StorageDead(_4);                 // scope 1 at <anon>:3:16: 3:16
-        _0 = ();                         // scope 1 at <anon>:1:11: 4:2
-        drop(_1) -> bb5;                 // scope 0 at <anon>:4:2: 4:2
+        StorageLive(_5);                 // scope 1 at <anon>:4:9: 4:14
+        StorageLive(_6);                 // scope 1 at <anon>:4:17: 4:23
+        StorageLive(_7);                 // scope 1 at <anon>:4:17: 4:23
+        StorageLive(_8);                 // scope 1 at <anon>:4:17: 4:20
+        _8 = &_1;                        // scope 1 at <anon>:4:17: 4:20
+        _7 = const std::ops::Index::index(_8, const 0usize) -> [return: bb5,
+unwind: bb3]; // scope 1 at <anon>:4:17: 4:23
     }
 
     bb5: {
-        StorageDead(_1);                 // scope 0 at <anon>:4:2: 4:2
-        return;                          // scope 0 at <anon>:4:2: 4:2
+        _6 = (*_7);                      // scope 1 at <anon>:4:17: 4:23
+        _5 = _6;                         // scope 1 at <anon>:4:17: 4:23
+        StorageDead(_6);                 // scope 1 at <anon>:4:23: 4:23
+        StorageDead(_8);                 // scope 1 at <anon>:4:23: 4:23
+        StorageDead(_7);                 // scope 1 at <anon>:4:24: 4:24
+        _0 = ();                         // scope 2 at <anon>:1:11: 5:2
+        StorageDead(_5);                 // scope 1 at <anon>:5:2: 5:2
+        drop(_1) -> bb6;                 // scope 0 at <anon>:5:2: 5:2
+    }
+
+    bb6: {
+        StorageDead(_1);                 // scope 0 at <anon>:5:2: 5:2
+        return;                          // scope 0 at <anon>:5:2: 5:2
     }
 }
 */
